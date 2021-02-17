bf7cbf0db4d55b3e2809c8ad1b679f742f04b24a
Alejandro Bernal <alexbernal32396@gmail.com>
2021-02-16 20:37:49 -0800

    Week 3 examples and todo list challenge.

---
+++ week-3/todo-list/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  // Store data using this array
+  public todos = ['Finish Math', 'Buy Groceries', 'Guitar Lessons'];
+  // How will you take the input
+  // Add functions as you fit!
+  // How do add data to an array in js? hint: search .push()
+
+}
