a85c7da5e8eeb88382bb09cf2d5aaeb759e1ce22
Alejandro Bernal <alexbernal32396@gmail.com>
2021-03-04 20:10:06 -0800

    Added new endpoints.

--- week-5/index.js
+++ week-5/index.js
@@ -14,6 +14,8 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
+mongoose.set('useNewUrlParser', true);
+
 // Connecting to mongodb
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -44,6 +46,63 @@ server.post('/add-journal', (req, res) => {
             message: 'Journal Created.',
             document: doc
         })
+    })
+});
+
+// Insert many Journal Entries
+// is is for your benefit if you ever need to have test data on your mongodb
+server.post('/add-many-journals', (req, res) => {
+    const incomingData = req.body.journals;
+
+    Journal.insertMany(incomingData, (err, doc) => {
+        if (err) {
+            res.status(500).send({
+                status: 500,
+                msg: 'Could not add journals'
+            });
+        }
+
+        res.status(200).send({
+            status: 200,
+            msg: 'All Journals.',
+            document: doc
+        })
+    });
+});
+
+server.get('/get-all-journals', (req, res) => {
+    Journal.find({}, (err, journals) => {
+        if (err) {
+            res.status(500).send({
+                status: 500,
+                msg: 'No Journals'
+            });
+        }
+        res.status(200).send({
+            status: 200,
+            msg: 'All journals found',
+            document: journals
+        });
+    })
+});
+
+// Remove journal entry
+server.post('/remove-journal', (req, res) => {
+    Journal.findOne({}, (err, doc) => {
+
+    })
+});
+
+// Remove by Id
+server.post('/remove-by-id', (req, res) => {
+    Journal.findByIdAndRemove({}, (err, doc) => {
+
+    })
+});
+
+server.post('/update-journal', (req, res) => {
+    Journal.findByIdAndUpdate({}, () => {
+
     })
 });
 
